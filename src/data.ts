7a6e5c91ed79a79294052cf0cb17723371b30314
Dionis <dionisenja@gmail.com>
2022-03-07 09:42:29 +0100

    dummy data

--- src/data.ts
+++ src/data.ts
@@ -1,20 +1,44 @@
 interface Data {
-    report: {
-        id: string,
-        source: string,
-        amount: number,
-        created_at: Date,
-        updated_at: Date,
-        type: ReportType
-    }[]
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
 }
 
-enum ReportType {
-    INCOME = "income",
-    EXPENSE = "expense",
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
 }
-
 
 export const data: Data = {
-    report: []
-}
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+        id: 'uuid2',
+        source: 'Youtube',
+        amount: 2000,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type: ReportType.INCOME,
+      }, 
+      {
+        id: 'uuid3',
+        source: 'Food',
+        amount: 500,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type: ReportType.EXPENSE,
+      },
+  ],
+};
