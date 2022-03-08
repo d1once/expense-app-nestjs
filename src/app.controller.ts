80fa9780f7068577aa70f2896c29a50b451ca7b7
Dionis <dionisenja@gmail.com>
2022-03-08 10:01:52 +0100

    update logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -46,8 +46,30 @@ export class AppController {
   }
 
   @Put(':id')
-  updateReport() {
-    return 'Updated';
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportToUpdate =  data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+      if(!reportToUpdate) return; 
+
+      const reportIndex = data.report.findIndex((report) => report.id === reportToUpdate.id)
+
+      data.report[reportIndex] = {
+         ...data.report[reportIndex],
+         ...body
+      }
+
+      return data.report[reportIndex] = {
+        ...data.report[reportIndex],
+        ...body
+     }
   }
 
   @Delete(':id')
