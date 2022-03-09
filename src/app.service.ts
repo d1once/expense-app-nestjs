1496c2d6f4facfab286e94ebb0ed9a6469b73001
Dionis <dionisenja@gmail.com>
2022-03-09 12:17:16 +0100

    logic for other http verbs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,67 @@
-import {Injectable} from "@nestjs/common"
-import {ReportType, data} from "./data"
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { ReportType, data } from './data';
+
+interface Report {
+  amount: number;
+  source: string;
+}
 
 @Injectable()
-export class AppService  {
+export class AppService {
   getAllReport(type: ReportType) {
     return data.report.filter((report) => report.type === type);
+  }
 
+  getReportById(type: ReportType, id: string) {
+    return data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+  }
+
+  createReport(type: ReportType, { amount, source }: Report) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
+    data.report.push(newReport);
+    return newReport;
+  }
+
+  updateReport(type: ReportType, id: string, body: Report) {
+    const reportToUpdate = data.report
+      .filter((report) => report.type === type)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date(),
+    };
+
+    return (data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    });
+  }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+
+    return;
   }
 }
