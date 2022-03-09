2368d011b676395e598774618f9721b78127de40
Dionis <dionisenja@gmail.com>
2022-03-09 12:05:20 +0100

    get all report service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
+import {Injectable} from "@nestjs/common"
 import {ReportType, data} from "./data"
 
-
+@Injectable()
 export class AppService  {
   getAllReport(type: ReportType) {
     return data.report.filter((report) => report.type === type);
